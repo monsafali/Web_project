d05bb5cfbc3eac2770e870330d1aaf0b6b5d0779
monsafali <monsafali272@gmail.com>
2024-06-26 17:39:36 +0500

    something new changing

--- health_care/app.js
+++ health_care/app.js
@@ -23,10 +23,3 @@ let btn = document.getElementById("clickbtn");
 
 
 
-
-let  labels = document.querySelectorAll('.form-control label')
-labels.forEach( label => {
-    label.innerHTML = label.innerText
-    .split('').map((letter, idx) => `<span style="transition-delay:${idx *50}ms">${letter}</span>`).join('')
-})
-
